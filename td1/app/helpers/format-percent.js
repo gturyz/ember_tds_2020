65606a2d0781a89672ab3c9af28873c94e0fb19b
Bonhême--Morsent Léo <gturyz@gmail.com>
2020-02-05 14:49:48 +0100

    up branch master

--- td1/app/helpers/format-percent.js
+++
@@ -1,5 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function formatPercent(params/*, hash*/) {
-  return `${params*100}%`;
-});
--- td1/app/helpers/plural.js
+++
@@ -1,5 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function plural(params/*, hash*/) {
-  return params>=2?'s':'';
-});
